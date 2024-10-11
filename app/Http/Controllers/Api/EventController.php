c550a98cf281e0ee5b5fe6cf47c0291d6d516a4b
Tolotra David <tolotradavid2@gmail.com>
2024-10-11 15:31:05 +0300

    Postman REST Client Crash Course

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -12,7 +12,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        return \App\Models\Event::all();
     }
 
     /**
@@ -26,9 +26,9 @@ class EventController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(\App\Models\Event $event)
     {
-        //
+        return $event;
     }
 
     /**
